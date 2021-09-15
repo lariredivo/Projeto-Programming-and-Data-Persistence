a4e3bb4ccfa0cd448b1b57449dff1f109cf185c3
lariredivo <larissa.redivo@gmail.com>
2021-09-14 22:20:19 -0300

    upload form

---
+++ Funcionarios2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Funcionarios2
+{
+    public partial class Funcionários : Form
+    {
+        public Funcionários()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "funcionarios";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            tbNome.Clear();
+            tbSetor.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Funcionários_Load(object sender, EventArgs e)
+        {
+            atualizaGrid();
+        }
+
+        private void atualizaGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM funcionario WHERE ativoFuncionario = 1";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dataGridView1.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//SETOR DA EMPRESA
+                    row.Cells[3].Value = reader.GetString(3);//NASCIMENTO
+                    row.Cells[4].Value = reader.GetString(4);//CONTRATAÇÃO
+                    dataGridView1.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+
+                comandoMySql.CommandText = "INSERT INTO funcionario (nomeFuncionario, dataNascimento, dataContratacao, setorEmpresa) " +
+                    "VALUES('" + tbNome.Text + "', '" + Convert.ToInt32(dtpNascimento.Text) + "','" + Convert.ToInt32(dtpContratacao.Text) + "', " + tbSetor.Text + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Inserido com sucesso");
+                atualizaGrid();
+                //limparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void tbNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
